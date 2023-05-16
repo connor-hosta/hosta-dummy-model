ad12896dd7914e67f20b8ba586cdda330ad3e336
connor-hosta <cwhelan@hosta.ai>
2023-05-16 13:49:50 -0400

    fix pre build script logic

--- bin/pre-build.sh
+++ bin/pre-build.sh
@@ -2,12 +2,12 @@
 image_details=$(aws ecr describe-images --repository-name $MODEL_NAME --filter "tagStatus=TAGGED" --query "imageDetails[?contains(imageTags, PROJECT_VERSION)]" --output json)
 
 # Check if image_details is not empty
-if [[ -n "$image_details" ]]; then
+if [[ "$image_details" != "[]" ]]; then
   # Extract the image digest of the first image in the list
   image_digest=$(echo "$image_details" | jq -r '.[0].imageDigest')
 
   # Update the tag of the image to "latest"
-  aws ecr put-image-tagging --repository-name $MODEL_NAME --image-digest "$image_digest" --image-tag latest
+  aws ecr put-image --repository-name $MODEL_NAME --image-digest "$image_digest" --image-tag latest
 
   # terminate build
   aws codebuild stop-build --id ${CODEBUILD_BUILD_ID}
