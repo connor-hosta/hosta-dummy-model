27d1e1a5d7f2e2cf3fe925b571cb3b780991f1af
connor-hosta <cwhelan@hosta.ai>
2023-05-16 09:51:05 -0400

    pre build script

---
+++ bin/pre-build.sh
@@ -0,0 +1,11 @@
+# Get the image details with the specified tag
+image_details=$(aws ecr describe-images --repository-name $MODEL_NAME --filter "tagStatus=TAGGED" --query "imageDetails[?contains(imageTags, PROJECT_VERSION)]" --output json)
+
+# Check if image_details is not empty
+if [[ -n "$image_details" ]]; then
+  # Extract the image digest of the first image in the list
+  image_digest=$(echo "$image_details" | jq -r '.[0].imageDigest')
+
+  # Update the tag of the image to "latest"
+  aws ecr put-image-tagging --repository-name $MODEL_NAME --image-digest "$image_digest" --image-tag latest
+fi
